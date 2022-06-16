eb7c508758f3d022120bf7fc4139d93c3c6b899e
Greg <emailgrogala@gmail.com>
2022-06-16 13:37:26 +0200

    en work on this moment on notes component

---
+++ src/app/mock-notes.ts
@@ -0,0 +1,15 @@
+import { Note } from "./notes";
+import { NotesComponent } from "./notes/notes.component";
+
+export const NOTES: Note[] = [
+    {id:11,title:"Eleven Notes",description:"Eleven Notes Description to note"},
+    {id:12,title:"Twelve Notes",description:"Twelve Notes Description to note"},
+    {id:13,title:"Therteen Notes",description:"Therteen Notes Description to note"},
+    {id:14,title:"Fourteen Notes",description:"Fourteen Notes Description to note"},
+    {id:15,title:"Fivteen Notes",description:"Fivteen Notes Description to note"},
+    {id:16,title:"Sixteen Notes",description:"Sixteen Description to note"},
+    {id:17,title:"7 teen Notes",description:"7 Description to note"},
+    {id:18,title:"8 teen Notes",description:"8 Description to note"},
+    {id:19,title:"9 teen",description:"9 Description to note"},
+    {id:20,title:"Twelve Notes",description:"20 Description to note"},
+]
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Note } from '../notes';
+import { NOTES } from '../mock-notes';
 
 @Component({
   selector: 'app-notes',
@@ -7,15 +8,39 @@ import { Note } from '../notes';
   styleUrls: ['./notes.component.scss']
 })
 export class NotesComponent implements OnInit {
-  note: Note = {
-    id: 1,
-    title: "First note",
-    description: "test display for new first note"
-  };
+  // noteTest: Note = {
+  //   id: 1,
+  //   title: "First note",
+  //   description: "test display for new first note"
+  // };
+  notes = NOTES;
+  selectedNote?: Note;
+  isShown: boolean = false;
+  isHide: boolean = false;
+  storeShownNoteNumber:Number=0;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("Init : " + this.selectedNote)
   }
+  
+  onSelect(note:Note):void {
+    if((this.selectedNote = note)){
+      this.isShown =! this.isShown
 
+      if(this.isShown){
+        this.storeShownNoteNumber = this.selectedNote.id
+      } else {
+        this.storeShownNoteNumber = 0
+      }
+
+    }
+
+   
+    console.log("selectedNote: " + this.selectedNote.id);
+    console.log("isShown: " + this.isShown);
+    console.log("isHide: " + this.isHide + " note to hide: " + note.id);
+    console.log("showNoteNumber: " + this.storeShownNoteNumber);
+  }
 }
